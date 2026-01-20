d5b1b238564b0b84d71680a410c77f8282c53bcc
AnhQuoc <AnhQuoc@monsterpixel.us>
2026-01-20 11:38:35 +0700

    Initial Commit

---
+++ bLoopTV/bLoopTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  bLoopTVApp.swift
+//  bLoopTV
+//
+//  Created by Monster on 20/1/26.
+//
+
+import SwiftUI
+
+@main
+struct bLoopTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
